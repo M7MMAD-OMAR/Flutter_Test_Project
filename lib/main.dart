65b5022de5df0e22fccf28e0ee3294049e3f5ad3
muham <muhammadaaa727@gmail.com>
2023-01-13 21:25:57 +0300

    Expanded Layout Testing

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,35 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Layout Testing"),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: 300,
+        child: Column(
+          children: [
+            Expanded(child: Container(color: Colors.red), flex: 1,),
+            Expanded(child: Row(
+             children: [
+               Expanded(child: Column(
+                 children: [
+                   Expanded(child: Container(color: Colors.purple), flex: 1,),
+                   Expanded(child: Row(
+                     children: [
+                       Expanded(child: Container(color: Colors.black), flex: 3,),
+                       Expanded(child: Container(color: Colors.black26), flex: 1,),
+                     ],
+                   ), flex: 1,),
+                 ]), flex: 2,
+               ),
+               Expanded(child: Container(color: Colors.teal), flex: 1,),
+             ]), flex: 2,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
-
