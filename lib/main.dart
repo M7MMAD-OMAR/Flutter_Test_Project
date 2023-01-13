0895c3fc8bec3afa79603c4641029b991ebb7c77
muham <muhammadaaa727@gmail.com>
2023-01-13 21:16:10 +0300

    clearing Age App

--- lib/main.dart
+++ lib/main.dart
@@ -11,63 +11,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Age App',
+      title: 'Layout Testing',
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: const MyHomePage(title: 'Age App'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, title}) : super(key: key);
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var textFieldBirthYear = TextEditingController();
-  Widget BirthYear = Text("");
-
-  void getAge() => {
-        setState(() {
-          int by = num.parse(textFieldBirthYear.text.toString()).toInt();
-          if (by < 1900 || by > DateTime.now().year) {
-            BirthYear =
-                Text("Birth Year must between 1900 and now year");
-          } else {
-            BirthYear = Text("${DateTime.now().year - by}");
-          }
-        })
-      };
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Age App Demo"),
-      ),
-      body: Column(
-        children: [
-          TextField(
-            controller: textFieldBirthYear,
-            decoration: InputDecoration(
-              hintText: "Enter your Birth Year",
-              label: Text("Birth Year"),
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          ElevatedButton(onPressed: getAge, child: Text("Get Age")),
-          SizedBox(
-            height: 10,
-          ),
-          BirthYear,
-        ],
-      ),
-    );
+    return const Placeholder();
   }
 }
+
